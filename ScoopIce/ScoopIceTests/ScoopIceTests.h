be789bbdf4ddc4e9b2bbc0e229dfe5d14b8a8752
Robert DeNicola <thedizzle23@gmail.com>
2012-06-19 12:52:01 -0400

    commit first

---
+++ ScoopIce/ScoopIceTests/ScoopIceTests.h
@@ -0,0 +1,13 @@
+//
+//  ScoopIceTests.h
+//  ScoopIceTests
+//
+//  Created by Robert DeNicola on 6/19/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ScoopIceTests : SenTestCase
+
+@end
---
+++ ScoopIce/ScoopIceTests/ScoopIceTests.m
@@ -0,0 +1,32 @@
+//
+//  ScoopIceTests.m
+//  ScoopIceTests
+//
+//  Created by Robert DeNicola on 6/19/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "ScoopIceTests.h"
+
+@implementation ScoopIceTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ScoopIceTests");
+}
+
+@end
